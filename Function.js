a6ba735cf4365224efff0b7f212b6836d1599c68
Pablo Sainz <zapencio@gmail.com>
2022-10-03 16:41:44 +0200

    Estructura

--- Function.js
+++ Function.js
@@ -1,13 +1,7 @@
-
-document.getElementsbyId("miBoton").onclick =
-alert("hola");
-/*function ReyesMagos(fecha) {
-    var FechaReyes= new Date(6/0/2023);
-    let diferencia = (FechaReyes - fecha)/(1000*60*60*24);
-   
-    return diferencia;
+function ReyesMagos() {
+    document.write("has hecho clic")
 }
-
+/*
 document.getElementsbyId("2boton").onclick =
 document.write("pruba")
 
