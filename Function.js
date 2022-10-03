f1d940a42c2a800e4fc88aafa7548854befd7c6a
Pablo Sainz <zapencio@gmail.com>
2022-10-03 16:17:37 +0200

    Primer commit

---
+++ Function.js
@@ -0,0 +1,16 @@
+
+document.getElementsbyId("miBoton").onclick =
+alert("hola");
+/*function ReyesMagos(fecha) {
+    var FechaReyes= new Date(6/0/2023);
+    let diferencia = (FechaReyes - fecha)/(1000*60*60*24);
+   
+    return diferencia;
+}
+
+document.getElementsbyId("2boton").onclick =
+document.write("pruba")
+
+function calcularArea(radio){
+return 2 * Math.PI * radio;
+}*/
