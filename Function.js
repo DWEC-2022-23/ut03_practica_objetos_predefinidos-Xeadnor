c7eeee718523c2c5745f688730b0ae31f95a8b10
Pablo Sainz <zapencio@gmail.com>
2022-10-07 23:24:33 +0200

    Primer ejercicio

--- Function.js
+++ Function.js
@@ -1,14 +1,28 @@
-function ReyesMagos() {
-    var nombre = document.getElementById("nombre").value;
-    var edad = parseInt(document.getElementById("edad").value);
+//#region Reyes Magos
+const fechaActual = document.getElementById("fechaActual");
+const butreyesMagos = document.getElementById("butreyesMagos");
+const fechasRestante = document.getElementById("fechaRestante");
+butreyesMagos.onclick = function () {
+    var fechaReyes = new Date();
+    let tiempo = 0;
+    fechaReyes.setFullYear(new Date().getFullYear() + 1);
+    fechaReyes.setHours(0, 0, 0);
+    fechaReyes.setDate(7);
+    fechaReyes.setMonth(0);
 
-    document.tarea.nombreobtenido.value = nombre;
-    document.tarea.edadobtenida.value = edad;
-}
-/*
-document.getElementsbyId("2boton").onclick =
-document.write("pruba")
+    var fecha = new Date(fechaActual.value);
+    tiempo = fechaReyes - fecha;
+    let tiempoFinal = Math.floor(tiempo / (1000 * 60 * 60 * 24));
+  let message;
 
-function calcularArea(radio){
-return 2 * Math.PI * radio;
-}*/
+  if (tiempoFinal < 0)
+    message = "Has viajado en el tiempoFinal y se te han pasado los reyes.";
+  else if (tiempoFinal == 0)
+    message = "¡Mira debajo de el árbol que han llegado hoy!";
+  else
+    message = "Faltan " + tiempoFinal + " días para que vengas los reyes.";
+
+  fechasRestante.innerHTML = message;
+};
+ //   document.tarea.nombreobtenido.value = nombre;
+  //  document.tarea.edadobtenida.value = edad;
